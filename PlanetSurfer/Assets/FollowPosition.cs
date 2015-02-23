9f443aff2aa4b84afa666988f214e5e1b4cf20c7
oPinon <olivier.pinon.mail@gmail.com>
2015-02-23 00:12:08 -0500

    Added a mini map

---
+++ PlanetSurfer/Assets/FollowPosition.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPosition : MonoBehaviour {
+
+	public GameObject ToFollow;
+	private Vector3 _offset;
+
+	// Use this for initialization
+	void Start () {
+		_offset = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.position = ToFollow.transform.position + _offset;
+	}
+}

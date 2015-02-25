7a6461f401aed3490e60d243b138ae456fbad1ec
oPinon <olivier.pinon.mail@gmail.com>
2015-02-25 04:54:52 -0500

    Added a goal area

---
+++ PlanetSurfer/Assets/MiniMap/PointTo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointTo : MonoBehaviour {
+
+	public GameObject Target;
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 posDiff = Target.transform.position - this.transform.position;
+		Quaternion newRotation = new Quaternion();
+		newRotation.SetLookRotation( Vector3.forward, posDiff );
+		this.transform.rotation = newRotation;
+	}
+}
---
+++ PlanetSurfer/Assets/Player/GoalArea.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalArea : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		Player player = other.GetComponent<Player>();
+		if(player != null) {
+			Debug.Log ("Entered goal");
+		}
+	}
+}

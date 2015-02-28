7d5a1282e8c9a377ccf1ec0109996b21e9423e81
oPinon <olivier.pinon.mail@gmail.com>
2015-02-28 18:04:51 -0500

    Added a menu scene (not scripted yet)

---
+++ PlanetSurfer/Assets/Menu/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	public float speed;
+
+	private float _angle = 0;
+
+	// Update is called once per frame
+	void Update () {
+	
+		_angle += speed*Time.deltaTime;
+
+		this.transform.eulerAngles = new Vector3(0,0,_angle);
+	}
+}
